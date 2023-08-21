095147f9cbce624fe2d92681d844a0400a131a7c
Nithin-6629 <140640603+Nithin-6629@users.noreply.github.com>
2023-08-21 20:11:00 +0530

    New about us

--- pages/AboutUs.py
+++ pages/AboutUs.py
@@ -4,33 +4,50 @@ col1,col2,col3 = st.columns([3,1,3])
 with col2:
 	st.header("About Us")
 st.divider()
-col1,col2,col3 = st.columns([1,20,1])
+col1,col2,col3 = st.columns([1,1,1])
 with col2:
-	st.markdown("##### We are a team driven by the problems that surround us and strive to provide a solution accessible to all. Our mission with this application is to provide a free-for-all service that enables users to know the stage of Alzheimer's Disease based on the uploaded MRI scan and suggestions to maintain your health based on the result given.")
+	st.image("clg-logo.jpg")
+col1,col2,col3 = st.columns([1,3,1])
+with col2:
+	st.markdown("##### This application is developed as a Mini project by the students of CVR College of Engineering.")
 st.divider()
-st.subheader("Introducing the team behind the project:")
-col1,col2,col3 = st.columns([2,1,2])
+st.subheader("Goal:")
+col1,col2 = st.columns([4,12])
+with col1:
+	st.image("adni-logo.jpg")
 with col2:
-	st.image("Jagan.jpg")
-# 	st.caption(":blue[Jagan Mohan K]")
-col1,col2,col3 = st.columns([5,1,5])
+	st.write("The primary motive for developing an application for early Alzheimer's detection using Convolutional Neural Networks (CNN) is to make a significant positive impact on the lives of individuals at risk of developing this devastating neurodegenerative disease. Early detection is crucial as it allows for timely intervention and improved patient outcomes. By leveraging the power of AI and deep learning through CNNs, the application aims to provide a non-invasive and cost-effective solution for early detection. This can empower medical professionals with a powerful tool to identify early warning signs of Alzheimer's in their patients, facilitating proactive treatment planning and access to support services. The motive behind developing this application is to contribute to the broader mission of improving the quality of life for those impacted by Alzheimer's, fostering a future where timely detection and intervention can potentially delay disease progression, enhance patient care, and bring us closer to finding a cure for this devastating condition.")
+st.divider()
+st.subheader("Developers:")
+st.write("We are a team driven by the problems that surround us and strive to provide a solution accessible to all. Our mission with this application is to provide a free-for-all service that enables users to know the stage of Alzheimer's Disease based on the uploaded MRI scan and suggestions to maintain your health based on the result given.")
+col1,col2,col3 = st.columns([3,3,3])
+with col1:
+	st.markdown("##### Abhinaya Rapolu")
 with col2:
-	st.markdown("###### Jagan Mohan K")
-col1,col2,col3 = st.columns([2,1,2])
+	st.markdown("##### Jagan Mohan Kandukuri")
+with col3:
+	st.markdown("##### Nithin Kumar Reddy Vangala")
+col1,col2,col3 = st.columns([3,3,3])
+with col1:
+	st.write("Roll No.: 20B81A6603")
 with col2:
-	st.image("Nithin.jpg")
-# 	st.caption(":red[Nithin Kumar Reddy V]")
-col1,col2,col3 = st.columns([7,2,7])
+	st.write("Roll No.: 20B81A6622")
+with col3:
+	st.write("Roll No.: 20B81A6629")
+col1,col2,col3 = st.columns([3,3,3])
+with col1:
+	st.write("Phone No.: 9100504749")
 with col2:
-	st.markdown("###### Nithin kumar reddy V")
-col1,col2,col3 = st.columns([2,1,2])
+	st.write("Phone No.: 9390822554")
+with col3:
+	st.write("Phone No.: 9502072420")
+col1,col2,col3 = st.columns([3,3,3])
+with col1:
+	st.write("20B81A6603@cvr.ac.in")
 with col2:
-	st.image("Abhinaya.jpg")
-# 	st.caption(":violet[Abhinaya Rapolu]")
-col1,col2,col3 = st.columns([8,2,8])
-with col2:
-	st.markdown("###### Abhinaya Rapolu")
-col1,col2,col3 = st.columns([8,1,8])
-with col2:
-	st.markdown("[Login](Login) ")
-	st.markdown("[Sign Up](Signup)")
+	st.write("20B81A6622@cvr.ac.in")
+with col3:
+	st.write("20B81A6629@cvr.ac.in")
+st.divider()
+st.subheader("Thank you for visiting our website")
+st.write("For any queries, Please email us through any of the addresses mentioned above")
